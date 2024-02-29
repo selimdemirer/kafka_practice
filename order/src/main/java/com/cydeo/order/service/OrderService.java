57a4434af1ce80d52321d6e2190006a339589d9d
selimdemirer <selimmdemirerr@gmail.com>
2024-02-29 21:12:38 +0300

    Kafka Basics are implemented

--- order/src/main/java/com/cydeo/order/service/OrderService.java
+++ order/src/main/java/com/cydeo/order/service/OrderService.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
     @KafkaListener(topics = "order")
     public void consumeMessage(String message){
         System.out.println("Message consumed, order will be created " + message);
     }
+
 }
